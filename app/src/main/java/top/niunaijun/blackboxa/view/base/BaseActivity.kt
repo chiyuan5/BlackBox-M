c73124438484af49e53dc4fa31a8420b867a406c
chiyuan5 <123289619+chiyuan5@users.noreply.github.com>
2026-03-29 10:09:12 +0800

    Update BaseActivity.kt

--- app/src/main/java/top/niunaijun/blackboxa/view/base/BaseActivity.kt
+++ app/src/main/java/top/niunaijun/blackboxa/view/base/BaseActivity.kt
@@ -1,17 +1,27 @@
 package top.niunaijun.blackboxa.view.base
 
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
-/**
- *
- * @Description:BaseActivity
- * @Author: wukaicheng
- * @CreateDate: 2021/5/4 15:58
- */
 open class BaseActivity : AppCompatActivity() {
 
-    protected fun initToolbar(toolbar: Toolbar,title:Int, showBack: Boolean = false, onBack: (() -> Unit)? = null) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    protected fun initToolbar(
+        toolbar: Toolbar,
+        title: Int,
+        showBack: Boolean = false,
+        onBack: (() -> Unit)? = null
+    ) {
         setSupportActionBar(toolbar)
         toolbar.setTitle(title)
         if (showBack) {
@@ -20,14 +30,35 @@ open class BaseActivity : AppCompatActivity() {
                 toolbar.setNavigationOnClickListener {
                     if (onBack != null) {
                         onBack()
+                    } else {
+                        onBackPressedDispatcher.onBackPressed()
                     }
-                    finish()
                 }
             }
         }
     }
 
-    protected fun currentUserID():Int{
-        return intent.getIntExtra("userID", 0)
+    protected fun applyToolbarInsets(toolbar: Toolbar) {
+        val initialTop = toolbar.paddingTop
+        ViewCompat.setOnApplyWindowInsetsListener(toolbar) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = initialTop + bars.top)
+            insets
+        }
+        ViewCompat.requestApplyInsets(toolbar)
     }
+
+    protected fun applyBottomInsets(vararg views: View) {
+        views.forEach { target ->
+            val initialBottom = target.paddingBottom
+            ViewCompat.setOnApplyWindowInsetsListener(target) { view, insets ->
+                val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.updatePadding(bottom = initialBottom + bars.bottom)
+                insets
+            }
+            ViewCompat.requestApplyInsets(target)
+        }
+    }
+
+    protected fun currentUserID(): Int = intent.getIntExtra("userID", 0)
 }
